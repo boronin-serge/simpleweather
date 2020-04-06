301afdd5b719ede40f189f40412a4b5cd98cc982
Sergey <boronin-serge@ya.ru>
2020-04-06 16:23:18 +0300

    simplify ModelConverters class

--- app/src/main/java/ru/boronin/simpleweather/data/databse/ModelConverters.kt
+++ app/src/main/java/ru/boronin/simpleweather/data/databse/ModelConverters.kt
@@ -5,39 +5,37 @@ import com.google.gson.Gson
 import com.google.gson.reflect.TypeToken
 import java.lang.reflect.Type
 
-class ModelConverters {
-  companion object {
-    var gson = Gson()
-    @TypeConverter
-    @JvmStatic
-    fun stringToSomeObjectList(data: String?): List<HourForecastEntity> {
-      if (data == null) {
-        return listOf()
-      }
-      val listType: Type = object : TypeToken<List<HourForecastEntity?>?>() {}.type
-      return gson.fromJson(data, listType)
+object ModelConverters {
+  var gson = Gson()
+  @TypeConverter
+  @JvmStatic
+  fun stringToSomeObjectList(data: String?): List<HourForecastEntity> {
+    if (data == null) {
+      return listOf()
     }
+    val listType: Type = object : TypeToken<List<HourForecastEntity?>?>() {}.type
+    return gson.fromJson(data, listType)
+  }
 
-    @TypeConverter
-    @JvmStatic
-    fun someObjectListToString(someObjects: List<HourForecastEntity?>?): String {
-      return gson.toJson(someObjects)
-    }
+  @TypeConverter
+  @JvmStatic
+  fun someObjectListToString(someObjects: List<HourForecastEntity?>?): String {
+    return gson.toJson(someObjects)
+  }
 
-    @TypeConverter
-    @JvmStatic
-    fun stringToDayObjectList(data: String?): List<DayForecastEntity> {
-      if (data == null) {
-        return listOf()
-      }
-      val listType: Type = object : TypeToken<List<DayForecastEntity?>?>() {}.type
-      return gson.fromJson(data, listType)
+  @TypeConverter
+  @JvmStatic
+  fun stringToDayObjectList(data: String?): List<DayForecastEntity> {
+    if (data == null) {
+      return listOf()
     }
+    val listType: Type = object : TypeToken<List<DayForecastEntity?>?>() {}.type
+    return gson.fromJson(data, listType)
+  }
 
-    @TypeConverter
-    @JvmStatic
-    fun someDayListToString(someObjects: List<DayForecastEntity?>?): String {
-      return gson.toJson(someObjects)
-    }
+  @TypeConverter
+  @JvmStatic
+  fun someDayListToString(someObjects: List<DayForecastEntity?>?): String {
+    return gson.toJson(someObjects)
   }
 }
